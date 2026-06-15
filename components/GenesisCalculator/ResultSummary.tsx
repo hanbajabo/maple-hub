59161c0cd11eb35e5b1fb2180873e8db0a06be06
Maple Hub Dev <dev@maplehub.com>
2026-06-15 18:30:13 +0900

    Fix ReferenceError for isPossibleToComplete in ResultSummary

--- components/GenesisCalculator/ResultSummary.tsx
+++ components/GenesisCalculator/ResultSummary.tsx
@@ -31,6 +31,7 @@ export default function ResultSummary({ result, season, isGenesisPass }: ResultS
         finalCompletionDate,
         finalWeekNumber,
         canCompleteInSeason,
+        isPossibleToComplete,
         daysUntilCompletion,
         daysUntilSeasonEnd,
     } = result;
